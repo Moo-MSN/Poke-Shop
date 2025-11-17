dfabd36baac2de1d5188a0f3a387bf949ba3540f
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-11-17 23:54:41 +0700

    Edit "image src to :src"

--- src/views/user/CartView.vue
+++ src/views/user/CartView.vue
@@ -27,7 +27,7 @@ const CartStore = useCartStore();
             <div class="flex items-center gap-3">
               <div class="avatar">
                 <div class="mask mask-squircle h-12 w-12">
-                  <img src="https://img.daisyui.com/images/profile/demo/2@94.webp" alt="Avatar Tailwind CSS Component" />
+                  <img :src="item.imageUrl" alt="Avatar Tailwind CSS Component" />
                 </div>
               </div>
               <div>
