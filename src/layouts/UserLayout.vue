b7c6cd51e1bfc64da53e3a3eeabb4b65ec4097c0
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-12-02 18:40:28 +0700

    Add "useCartStore" for use data in "cart.js"

--- src/layouts/UserLayout.vue
+++ src/layouts/UserLayout.vue
@@ -2,7 +2,11 @@
 import { RouterLink, useRouter } from "vue-router";
 import { ref, onMounted } from "vue";
 
+import { useCartStore } from "@/stores/user/cart";
+
 const router = useRouter();
+
+const cartStort = useCartStore();
 
 const isLoggedIn = ref(false);
 const searchText = ref("");
@@ -59,13 +63,13 @@ const handleSearch = (event) => {
                   d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                 />
               </svg>
-              <span class="badge badge-sm indicator-item">8</span>
+              <span class="badge badge-sm indicator-item"> {{ cartStort.summaryQuantity }}</span>
             </div>
           </div>
           <div tabindex="0" class="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow">
             <div class="card-body">
-              <span class="text-lg font-bold">8 Items</span>
-              <span class="text-info">Subtotal: $999</span>
+              <span class="text-lg font-bold">{{ cartStort.summaryQuantity }} Items</span>
+              <span class="text-info">Subtotal: ฿ {{ cartStort.summaryPrice }}</span>
               <div class="card-actions">
                 <RouterLink :to="{ name: 'cart' }" class="btn btn-primary btn-block">View cart</RouterLink>
               </div>
