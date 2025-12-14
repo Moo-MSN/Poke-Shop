26866aaa585c71adf9b3e8842d9b15a7cd19df5c
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-12-14 14:16:50 +0700

    Add "onMounted" for use CartStore in first loading page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <script setup>
 import { RouterView } from "vue-router";
+import { onMounted } from "vue";
+
+import { useCartStore } from "./stores/user/cart";
+
+const CartStore = useCartStore();
+
+onMounted(()=>{
+  CartStore.loadCart()
+})
 </script>
 
 <template>
