1aec260393dc3abfe15daf6bd73085d96a37a5f9
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-12-14 14:25:09 +0700

    add "addToCart " in defineProps for use in "StoreView.vue" when clicked "add to cart" to add product to "CartView.vue"

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -3,23 +3,24 @@ import { defineProps } from 'vue';
 import { RouterLink } from 'vue-router';
 
 defineProps({
-    products: Array
+    products: Array,
+    addToCart:Function
 })
 
 </script>
 
 <template>
   <section class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-5 m-2">
-    <div v-for="Product in products" class="card bg-base-100 w-auto shadow-sm">
+    <div v-for="product in products" class="card bg-base-100 w-auto shadow-sm">
       <figure>
-        <img :src="Product.imageUrl" />
+        <img :src="product.imageUrl" />
       </figure>
       <div class="card-body">
-        <h2 class="card-title">Poké : {{ Product.name }}</h2>
-        <p>{{ Product.about }}</p>
-        <p>price : {{ Product.price }} Poké</p>
+        <h2 class="card-title">Poké : {{ product.name }}</h2>
+        <p>{{ product.about }}</p>
+        <p>price : {{ product.price }} Poké</p>
         <div class="card-actions justify-end">
-          <RouterLink :to="{name: 'cart'}" class="btn btn-primary" >Add to cart</RouterLink>
+          <RouterLink :to="{name: 'cart'}" class="btn btn-primary" @click="addToCart(product)" >Add to cart</RouterLink>
         </div>
       </div>
     </div>
