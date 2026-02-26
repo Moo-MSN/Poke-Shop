b33b16b084903b2ccda1587d06cd4794d07691c1
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2026-02-26 14:59:46 +0700

    Add "path: /login"

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import SearchView from '@/views/user/SearchView.vue'
 import StoreView from '@/views/user/StoreView.vue'
 import SuccessView from '@/views/user/SuccessView.vue'
 import RegisterView from '@/views/user/RegisterView.vue';
+import Login from '@/views/user/Login.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -50,6 +51,11 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
     },
 
     // Admin
