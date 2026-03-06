112877d157ea4218f8c22cdfa1e9326a9e79ee03
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2026-03-06 16:32:11 +0700

    Create form "login" of admin site

--- src/views/admin/Admin LoginViwe.vue
+++ src/views/admin/Admin LoginViwe.vue
@@ -1,3 +1,14 @@
 <template>
-admin login
+  <div class="h-screen flex items-center">
+    <div class="max-w-xl flex shadow-xl mx-auto items-center justify-center p-8">
+      <div class="max-w-md w-full p-6shadow-md rounded">
+        <h2 class="text-2xl text-center font-bold mb-4">Admin Login</h2>
+        <form>
+          <input type="text" placeholder="Username" class="input input-bordered w-full mb-3" />
+          <input type="password" placeholder="Password" class="input input-bordered w-full mb-3" />
+          <button type="submit" class="btn btn-primary w-full">Login</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
