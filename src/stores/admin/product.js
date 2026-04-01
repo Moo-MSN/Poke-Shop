adb805f17a4f942b8c94fac39ce35decc34aed22
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2026-04-01 16:16:41 +0700

    Edit action for use in localstorage test brfore connect to DB

--- src/stores/admin/product.js
+++ src/stores/admin/product.js
@@ -1,26 +1,23 @@
 import { defineStore } from "pinia";
 import axios from "axios";
-import { Update } from "vite";
 
 // BaseUrl to DB
 const BASE_URL = "http://localhost:8000";
 
-export const useAdminProductStore = defineStore("product", {
+export const useAdminProductStore = defineStore("admin-products", {
   state: () => ({
-    list: [
-      {
-        name: "test",
-        imageUrl: "123",
-        price: "111",
-        quantity: "20",
-        remainquantity: "11",
-        status: "open",
-        UpdatedAt: new Date().toLocaleString(),
-      },
-    ],
+    list: [],
   }),
   actions: {
-    async loadProduct() {
+    // mokc products ไว้ใน localstorage
+    loadProduct() {
+      const products = localStorage.getItem("admin-products");
+      if (products) {
+        this.list = JSON.parse(products);
+      }
+    },
+
+    /**  async loadProduct() {
       try {
         const res = await axios.get(`${BASE_URL}/products`);
         if (res) {
@@ -29,9 +26,14 @@ export const useAdminProductStore = defineStore("product", {
       } catch (err) {
         console.error("Error fethcing products", err);
       }
+    },*/
+
+    // Mock localstorage
+    getProduct(index) {
+      return this.list[index];
     },
     // getProduct ต้องใส่ index ด้วยเพื่อทำการระบุตำแหน่งของ product แต่ละตัว
-    async getProduct(index) {
+    /** async getProduct(index) {
       try {
         // ตรวจสอบว่ามี product ใน list
         if (this.list.length === 0) {
@@ -42,8 +44,16 @@ export const useAdminProductStore = defineStore("product", {
       } catch (err) {
         console.error("Error fethcing products", err);
       }
+    },*/
+
+    // Mock localstorage
+    addProduct(productData) {
+      productData.remainquantity = productData.quantity;
+      productData.UpdatedAt = new Date().toLocaleString();
+      this.list.push(productData);
+      localStorage.setItem("admin-products", JSON.stringify(this.list));
     },
-    async addProduct(productData) {
+    /** async addProduct(productData) {
       try {
         // กำหนดให้ quantity = remainquantity เมื่อทำการ update จำนวนสินค้า
         productData.remainquantity = productData.quantity;
@@ -55,8 +65,26 @@ export const useAdminProductStore = defineStore("product", {
       } catch (err) {
         console.error("Error Adding products", err);
       }
+    }, */
+
+    // Mock localstorage
+    updateProduct(index, productData) {
+      Object.assign(this.list[index], productData, {
+        remainquantity : productData.quantity, // รีเซต quantity ให้เท่ากันเมื่อทำการ updateProduct
+        UpdatedAt: new Date().toLocaleString(),
+      });
+      // ใส่ field Data ที่ต้องการ update เหมือนจะใช้ตัวนี้ในการ updateProduct เพราะเรา index เป็น key ในการดึง Data
+      /**  this.list[index].name = productData.name;
+      this.list[index].imageUrl = productData.imageUrl;
+      this.list[index].price = productData.price;
+      this.list[index].quantity = productData.quantity;
+      this.list[index].remainquantity = productData.remainquantity;
+      this.list[index].UpdatedAt = new Date().toLocaleString();*/
+      localStorage.setItem("admin-products", JSON.stringify(this.list));
+      console.log("Updated list", this.list);
     },
-    async UpdateProduct(index, productData) {
+
+    /** async updateProduct(index, productData) {
       try {
         // ใส่ field Data ที่ต้องการ update
         this.list[index].name = productData.name;
@@ -74,8 +102,15 @@ export const useAdminProductStore = defineStore("product", {
       } catch (err) {
         console.error("Error Updeting products", err);
       }
+    },*/
+
+    // Mock localstorage
+    removeProduct(index) {
+      this.list.splice(index, 1);
+      localStorage.setItem("admin-products", JSON.stringify(this.list));
     },
-    async removeProduct(index) {
+
+    /** async removeProduct(index) {
       try {
         // ดึง id ของ product ที่จะลบ
         const productId = this.list[index].id;
@@ -89,6 +124,6 @@ export const useAdminProductStore = defineStore("product", {
       } catch (err) {
         console.error("Error Remove products", err);
       }
-    },
+    },*/
   },
 });
