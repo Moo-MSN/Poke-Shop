970b050a8d17bb23eeaa3d7d46a5e07c8ba8fb4a
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2026-04-02 12:14:11 +0700

    Edit Tabel by import "Table" components

---
+++ src/components/Table.vue
@@ -0,0 +1,24 @@
+<!-- สร้าง Tabel ไว้ใช้กับ back office -->
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+  Headers: Array,
+});
+</script>
+
+<template>
+  <div class="overflow-x-auto">
+    <table class="table">
+      <!-- head -->
+      <thead>
+        <tr class="text text-center">
+          <th v-for="Header in Headers">{{ Header }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <slot></slot>
+      </tbody>
+    </table>
+  </div>
+</template>
