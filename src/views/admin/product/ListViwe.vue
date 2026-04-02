f64d5f027f857804422a9e1e126a04473ce4b99c
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2026-04-02 12:13:21 +0700

    Create "Table" components for use in back office

--- src/views/admin/product/ListViwe.vue
+++ src/views/admin/product/ListViwe.vue
@@ -10,6 +10,7 @@ import AdminLayout from "@/layouts/AdminLayout.vue";
 // icon
 import Trash from "@/components/icons/Trash.vue";
 import Edit from "@/components/icons/Edit.vue";
+import Table from "@/components/Table.vue";
 
 const adminProductStore = useAdminProductStore();
 
@@ -29,47 +30,31 @@ const removeProduct = (index) => {
       <div class="text-2xl font-semibold">Products</div>
       <RouterLink :to="{ name: 'admin-products-create' }" class="btn btn-neutral btn-xs sm:btn-sm md:btn-md">Add New</RouterLink>
     </div>
-    <div class="overflow-x-auto">
-      <table class="table">
-        <!-- head -->
-        <thead>
-          <tr class="text text-center">
-            <th>Name</th>
-            <th>Image</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Status</th>
-            <th class="w-50">UpdatedAt</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <!-- row 1 --><!-- index ใช้ระบุตำแหน่งข้อมูลในการแก้ไขหรือลบ product -->
-          <tr v-for="(product, index) in adminProductStore.list" class="text text-center">
-            <th>{{ product.name }}</th>
-            <td>
-              <img :src="product.imageUrl" class="w-20 mx-auto" />
-            </td>
-            <td>{{ product.price }}</td>
-            <td>{{ product.remainquantity }}/{{ product.quantity }}</td>
-            <td>
-              <div class="badge" :class="product.status === 'open' ? 'badge-success' : 'badge-error'">
-                {{ product.status }}
-              </div>
-            </td>
-            <td class="w-50">{{ product.UpdatedAt }}</td>
-            <td>
-              <RouterLink :to="{ name: 'admin-products-update', params: { id: index } }" class="btn btn-square btn-ghost">
-                <Edit></Edit>
-              </RouterLink>
+    <Table :Headers="['Name','Image','Price','Quantity','Status','Updated At','Action']">
+      <!-- row 1 --><!-- index ใช้ระบุตำแหน่งข้อมูลในการแก้ไขหรือลบ product -->
+      <tr v-for="(product, index) in adminProductStore.list" class="text text-center">
+        <th>{{ product.name }}</th>
+        <td>
+          <img :src="product.imageUrl" class="w-20 mx-auto" />
+        </td>
+        <td>{{ product.price }}</td>
+        <td>{{ product.remainquantity }}/{{ product.quantity }}</td>
+        <td>
+          <div class="badge" :class="product.status === 'open' ? 'badge-success' : 'badge-error'">
+            {{ product.status }}
+          </div>
+        </td>
+        <td class="w-50">{{ product.UpdatedAt }}</td>
+        <td>
+          <RouterLink :to="{ name: 'admin-products-update', params: { id: index } }" class="btn btn-square btn-ghost">
+            <Edit></Edit>
+          </RouterLink>
 
-              <button class="btn btn-square btn-ghost" @click="removeProduct(index)">
-                <Trash></Trash>
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+          <button class="btn btn-square btn-ghost" @click="removeProduct(index)">
+            <Trash></Trash>
+          </button>
+        </td>
+      </tr>
+    </Table>
   </AdminLayout>
 </template>
