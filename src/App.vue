9bfcdad125a0d5bd37c835cfabbefb57103a1e0a
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2026-04-14 18:54:09 +0700

    Create "event.js" and import "event" to App.vue page for alert to admin when ''edit or update data"

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,28 @@ import { onMounted } from "vue";
 
 import { useCartStore } from "./stores/user/cart";
 import { useProductStore } from "./stores/user/product";
+import { useEventStore } from "./stores/event";
 
 const CartStore = useCartStore();
 const productStore = useProductStore();
+// ใช้ Toast ในการบอก admin ว่าการแก้ไขหรืออัพเดตเรียบร้อยแล้ว
+const eventStore = useEventStore();
 
 onMounted(async () => {
   CartStore.loadCart();
   await productStore.fetchProduct();
+  // Test Toast
+  //eventStore.popupMessage("success", "Test Message");
 });
 </script>
 
 <template>
+  <!-- Toast แสดงเฉพาะตอนที่ eventStore.alert เป็น true -->
+  <div v-if="eventStore.alert" class="toast">
+    <div class="alert" :class="`alert-${eventStore.data.status}`">
+      <span>{{ eventStore.data.message }}</span>
+    </div>
+  </div>
+
   <RouterView />
 </template>
