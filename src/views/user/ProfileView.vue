43bb4976d9e3fad5ee9994fcd14f3890067e45ae
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2026-04-29 22:52:00 +0700

    add "vue, vue-router, useUserStore" and create onmounted "getUserbyId" to get data from DB for show in a page

--- src/views/user/ProfileView.vue
+++ src/views/user/ProfileView.vue
@@ -1,19 +1,35 @@
 <script setup>
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+
 import UserLayout from "@/layouts/UserLayout.vue";
+import { useUserStore } from "@/stores/user/user";
+
+const userStore = useUserStore();
+const route = useRoute();
+const user = ref("");
+const previewUrl = ref("");
+
+onMounted(async () => {
+  const userId = route.params.id; // ใช้ id จาก route params
+  user.value = await userStore.getUserbyId(userId);
+  // แสดง image profile ของ  user
+  previewUrl.value = user.value?.imageurl;
+});
 </script>
 
 <template>
   <UserLayout>
     <div class="max-w-3xl mx-auto border border-base-200 shadow-xl p-8 my-4">
-      <div class="text-2xl font-semibold">Your profile ต้องดึงข้อมูลจาก local ในหน้า register</div>
+      <div class="text-2xl font-semibold">Your profile</div>
       <div class="flex flex-col items-center">
         <div class="flex flex-col items-center">
           <div class="avatar">
             <div class="w-24 rounded-full">
-              <img src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp" />
+              <!-- ถ้ามีรูปจาก DB จะใช้รูปนั้น ถ้าเปลี่ยนไฟล์ใหม่ก็จะ preview -->
+              <img :src="previewUrl || '/src/components/icons/default-profile.png'" />
             </div>
           </div>
-          <input type="file" class="file-input file-input-ghost file-input-sm mt-4" />
         </div>
       </div>
       <div class="divider"></div>
@@ -22,36 +38,36 @@ import UserLayout from "@/layouts/UserLayout.vue";
         <div>
           <fieldset class="fieldset">
             <label class="text-base font-semibold">First Name</label>
-            <div class="text-base">รอดึงจาก db</div>
+            <div class="text-base">{{ user.firstname }}</div>
           </fieldset>
         </div>
         <div>
           <fieldset class="fieldset">
             <label class="text-base font-semibold">Last Name</label>
-            <div class="text-base">รอดึงจาก db</div>
+            <div class="text-base">{{ user.lastname }}</div>
           </fieldset>
         </div>
-         <div>
+        <div>
           <fieldset class="fieldset">
             <label class="text-base font-semibold">E-mail</label>
-            <div class="text-base">รอดึงจาก db</div>
+            <div class="text-base">{{ user.email }}</div>
           </fieldset>
         </div>
-         <div>
+        <div>
           <fieldset class="fieldset">
             <label class="text-base font-semibold">Date of Birth</label>
-            <div class="text-base">รอดึงจาก db</div>
+            <div class="text-base">{{ user.birthday }}</div>
           </fieldset>
         </div>
-         <div>
+        <div>
           <fieldset class="fieldset">
             <label class="text-base font-semibold">Address</label>
-            <div class="text-base">รอดึงจาก db</div>
+            <div class="text-base">{{ user.address }}{{ user.zipcode }}</div>
           </fieldset>
         </div>
       </div>
       <div class="flex flex-col mt-5 mx-auto">
-        <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl ">Edit Your Profile</button>
+        <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">Edit Your Profile</button>
       </div>
     </div>
   </UserLayout>
